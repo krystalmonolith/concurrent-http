7e270ac9f544606352f1bd462a615ea40385f22d
Mark Deazley <mdeazley@gmail.com>
2021-08-17 07:59:03 -0400

    Added three tabs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {MatSliderChange} from '@angular/material/slider';
+import {ThemePalette} from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'concurrent-http';
+  @ViewChild('sliderText') sliderText!: ElementRef;
+
+  links = ['RxJS concat','RxJS merge','RxJS merge with retry'];
+  activeLink = this.links[0];
+  background: ThemePalette = undefined;
+
+  ngOnInit() {
+    this.background = 'primary';
+  }
+
+  toggleBackground() {
+    this.background = this.background ? undefined : 'primary';
+  }
+
+  sliderChange($event: MatSliderChange) {
+    console.log(`SliderChange: ${$event.value}`);
+    this.sliderText.nativeElement.value = `${$event.value}`;
+  }
+
+  sliderValueChange($event: MatSliderChange) {
+    console.log(`SliderValue: ${$event.value}`);
+    this.sliderText.nativeElement.value = `${$event.value}`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatSliderModule} from '@angular/material/slider';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
