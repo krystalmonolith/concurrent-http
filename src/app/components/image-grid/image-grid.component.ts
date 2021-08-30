77ad51357f94c25e23c757f7687c4b24a4e68783
Mark Deazley <mdeazley@gmail.com>
2021-08-30 17:26:16 -0400

    Added image count, elapsed time, and progress bar. Upped version to 1.2.

--- src/app/components/image-grid/image-grid.component.ts
+++ src/app/components/image-grid/image-grid.component.ts
@@ -11,9 +11,12 @@ import {Base64ArrayBuffer} from './Base64ArrayBuffer';
 export class ImageGridComponent implements OnInit {
 
   images: Array<string> = [];
+  imageCount = 0;
+  imagePercentage = 0;
   imageFetchError?: string;
   imageFetchErrorFilename?: string;
-
+  loadStartTime = 0;
+  elapsedTimeMsec = 0;
 
   @Input() title: string = '';  // The title string for the current tab.
 
@@ -30,7 +33,6 @@ export class ImageGridComponent implements OnInit {
   //
   // Could DI and a Image Storage service pattern help us here?
   //
-
   @Input() loadImageDelegate?: (fileList: Array<string>,  // The list of files to be rendered.
                                 imageGridComponent: ImageGridComponent, // The correct 'this' for pushing images: See member function pushImage() below.
                                 imagePusher: (imageGridComponent: ImageGridComponent, fileResponse: ArrayBuffer) => void) => Observable<void>; // The image pushing function.
@@ -45,13 +47,18 @@ export class ImageGridComponent implements OnInit {
   loadList(): void {
     // Clear out the old images and error information.
     this.images = [];
+    this.imageCount = 0;
+    this.imagePercentage = 0;
     this.imageFetchError = undefined;
     this.imageFetchErrorFilename = undefined;
+    this.elapsedTimeMsec = 0;
 
     // Fetch the list of files from FileService.
     this.fileService.getFileList()
       .subscribe(fileList => {
         // Call the image loading delegate function to attempt to load all the images in the 'fileList'.
+        this.imageCount = fileList.length;
+        this.loadStartTime = Date.now();
         this.loadImageDelegate!(fileList, this, this.pushImage).subscribe({
           error: err => {
             this.imageFetchError = `${err['status']} ${err['statusText']}`;
@@ -67,6 +74,9 @@ export class ImageGridComponent implements OnInit {
     // NOT ImageGridComponents's 'this'!
     //
     // this.images.push('data:image/png;base64,' + Base64ArrayBuffer.encode(fileResponse)); // NO!!!!
+
+    imageGridComponent.imagePercentage = imageGridComponent.imageCount > 0 ? imageGridComponent.images.length * 100 / imageGridComponent.imageCount : 0;
+    imageGridComponent.elapsedTimeMsec = Date.now() - imageGridComponent.loadStartTime;
   }
 
   refresh() {
